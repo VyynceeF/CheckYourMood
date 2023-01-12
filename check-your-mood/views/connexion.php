ecfad68c5571d6c0837c52471a0c38bf6ba80798
Jules Blanchard <jules.blanchard@iut-rodez.fr>
2023-01-12 02:34:16 +0100

    Mise en forme de la page connexion de manière responsive (fini) (Ajout d'un fichier css pour la page connexion)

--- check-your-mood/views/connexion.php
+++ check-your-mood/views/connexion.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CheckYourMood - Connexion</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="/check-your-mood/css/style.css">
+    <link rel="stylesheet" href="/check-your-mood/css/connexion.css">
 </head>
 <?php
 spl_autoload_extensions(".php");
@@ -16,52 +16,50 @@ use yasmf\HttpHelper;
 
 ?>
     <body class="backgroundBleu">
-        <script src="script\script.js"></script>
-        <div class="conteneur-main">
-            <div class="conteneur">
-                <!---Cadre de connexion-->
-                <div class="login">
+    <script src="script\script.js"></script>
+        <div class="bigContainer">
+            <div class="container">
+                <div class="row ligne">
+                    <div class="col-md-6 bigLeft">
+                        <div class="wrapper">
+                            <form action="" method="post">
+                                <span class="titre"><h1>Connexion</h1></span>
+                                <?php if(isset($_POST['premiereCo'])){?>
+                                    <p class = "err">Mot de passe ou identifiant incorrecte</p>
+                                <?php } ?>
+                                <div class="wrapperForm">
+                                    <div class="formulaireCo">
+                                        <input type="hidden" name="premiereCo" value="1">
+                                        <input type="hidden" name="action" value="login">
+                                        <div class="logoInput">
+                                            <span class="glyphicon glyphicon-user logoConnexion"></span>
+                                            <input class="form-control" type="text" name="identifiant" placeholder="Identifiant" value="<?php echo HttpHelper::getParam('identifiant'); ?>">
+                                        </div>
+                                        <div class="contain-mdp">
+                                            <div class="logoInput">
+                                                <span class="glyphicon glyphicon-lock logoConnexion"></span>
+                                                <input class="form-control" type="password" name="motdepasse" placeholder="Mot de passe" id="myInput">
+                                            </div>
+                                            <div class="contain-revele">
+                                                <input class="checkInput" type="checkbox" name="revele" onclick="myFunction()">
+                                                <span class="revelerMDP">Révéler mot de passe</span>
+                                            </div>
+                                        </div><br>
+                                    </div>
+                                    <div class="btn-connect">
+                                        <button class="btn" type="submit">Se connecter</button>
+                                        <span>Ou&nbsp;<a href="/check-your-mood?controller=home&action=goTo&namepage=inscription"> s'inscrire</a></span> 
+                                    </div>
+                                </div>
 
-                    <p class="login-text">Connexion</p>
-
-                    <?php if(isset($_POST['premiereCo'])){?>
-                        <p class = "err">Mot de passe ou identifiant incorrecte</p>
-                    <?php } ?>
-
-                    <form action="index.php" method="post">
-                        <input type="hidden" name="premiereCo" value="1">
-                        <input type="hidden" name="action" value="login">
-                        <div class="logoInput">
-                            <span class="glyphicon glyphicon-user logoConnexion"></span>
-                            <input class="form-control" type="text" name="identifiant" placeholder="Identifiant" value="<?php echo HttpHelper::getParam('identifiant'); ?>">
+                            </form>
                         </div>
-                        <div class="contain-mdp">
-                            <div class="logoInput">
-                                <span class="glyphicon glyphicon-lock logoConnexion"></span>
-                                <input class="form-control" type="password" name="motdepasse" placeholder="Mot de passe" id="myInput">
-                            </div>
-                            <div class="contain-revele">
-                                <input class="checkInput" type="checkbox" name="revele" onclick="myFunction()">
-                                <span class="revelerMDP">Révéler mot de passe</span>
-                            </div>
-                        </div>
-                        <div class="btn-connect"><button class="btn" type="submit">Se connecter</button></div>
-                    </form>
-
-                    <br>
-                    <hr>
-
-                    <div class="contain-bottom">    
-                        <p>Vous n'avez pas de compte ? <a href="/check-your-mood?controller=home&action=goTo&namepage=inscription">Inscrivez-vous</a></p>    
                     </div>
-                    
+        
+                    <div class="col-md-6 bigRight">
+                        <img class="logo" src="/check-your-mood/images/CheckYourMoodLogo.png" alt="Logo Check Your Mood">
+                    </div>
                 </div>
-
-
-                <div class="login right">
-                    <img class="logo" src="/check-your-mood/images/CheckYourMoodLogo.png" alt="Logo Check Your Mood">
-                </div>
-
             </div>
         </div>
     </body>
